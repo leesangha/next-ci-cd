6ed870317e5abd04832bde41d22070a5833843c5
leesangha <sanghalee1@naver.com>
2023-05-23 09:39:21 +0900

    script

--- nextjs-pm2-deploy.sh
+++ nextjs-pm2-deploy.sh
@@ -4,16 +4,20 @@ npm ci --legacy-peer-deps
 
 BUILD_DIR=temp npm run build --if-present || exit
 
-if [! -d "temp"];then
+if [ ! -d "temp" ]; then
     echo '\033[31m temp Directory not exists!\033[0m'
     exit 1;
 fi
 
 rm -rf .next
 
+echo `ls -al`
+
 mv temp .next
 
-pm2 reload example --update-env
+echo `ls -al`
+
+pm2 reload example
 
 echo "Deploy done."
 
